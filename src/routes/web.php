9eb7915f2aa4712b7574742cee90b820a43f79ba
hosomi tadashi <hosomitadasi@gmail.com>
2025-06-07 01:08:52 +0900

    勤怠用のcontroller作成、一般ユーザー用のviewも作成開始。明日は本格的に認証の実装と勤怠部分を作成予定。

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,18 +1,37 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\AttendanceController;
+use App\Http\Controllers\ListController;
+use App\Http\Controllers\RestController;
+use Laravel\Fortify\Http\Controllers\AuthenticatedSessionController;
+use Laravel\Fortify\Http\Controllers\RegisteredUserController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('login', [AuthenticatedSessionController::class, 'store']);
+Route::post('/register', [RegisteredUserController::class, 'store']);
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/attendance', [AttendanceController::class, 'getAttendance']);
+    // 勤怠登録画面表示
+    Route::get('/attendance/start', [AttendanceController::class, 'startAttendance']);
+    // 勤務開始処理
+    Route::get('/attendance/end', [AttendanceController::class, 'endAttendance']);
+    // 勤務終了処理
+
+    Route::get('/break/start', [RestController::class, 'startRest']);
+    // 休憩開始処理
+    Route::get('/break/end', [RestController::class, 'endRest']);
+    // 休憩終了処理
+
+    Route::get('/attendance/list', [ListController::class, 'getList']);
+    // 勤怠一覧画面表示
+    Route::get('/attendance/{id}', [ListController::class, 'detail']);
+    // 勤怠詳細画面表示
+});
+// 一般ユーザー用のミドルウェア
+
+// 管理者用のミドルウェア
